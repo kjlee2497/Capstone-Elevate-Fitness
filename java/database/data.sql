17d38a044ea2de7780fa628ebacc9656654ad74c
Jose <rev.jose66@gmail.com>
2023-09-25 14:47:25 -0700

    Login & Registration Design

--- java/database/data.sql
+++ java/database/data.sql
@@ -8,5 +8,5 @@ INSERT INTO users (username,password_hash,role) VALUES ('jose','$2a$10$gPzzMX6n6
 INSERT INTO users (username,password_hash,role) VALUES ('alejandro', '$2a$10$kEvamjfY0ju9Hme4llBH2eJ0KWbqP/QrK0B7WrSe14zGfr5uvXXlq', 'ROLE_TRAINER');
 INSERT INTO users (username,password_hash,role) VALUES ('caroline', '$2a$10$VmhtTJg3l9WMi2tXNMUQpe0oT4tpZNUfPCLhk0YXgxHIQdcDOya22', 'ROLE_TRAINER');
 
-
+INSERT INTO
 COMMIT TRANSACTION;
---
+++ vue/src/views/Metrics.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="metrics" class="text-center">
+        <form @submit.prevent="metrics">
+            <h1>User Info</h1>
+            <div class="form-input-group">
+                <label for="userHeight">Height</label>
+            </div>
+            <div class="form-input-group">
+                <label for="userWeight">Weight</label>
+            </div>
+            <div class="form-input-group">
+                <label for="userAge">Age</label>
+            </div>
+            <button type="submit">submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+</script>
