aa4921c8cab75a5f4e42fc003ed10c67e8fe4d5e
Jose <rev.jose66@gmail.com>
2023-10-01 10:26:05 -0700

    scrollbar styling added to pages

--- vue/src/views/EditExercise.vue
+++ vue/src/views/EditExercise.vue
@@ -4,7 +4,9 @@
      style="background-image: url('https://images.unsplash.com/photo-1674834727206-4bc272bfd8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80');" >
  
   <div>
-     <h1>Edits</h1>
+    <br>
+    <br>
+     <h1>Edit Exercise</h1>
     <exercise-edit-form v-bind:exerciseId="this.exerciseId" />
   </div>
 </div>
@@ -24,7 +26,13 @@ export default {
 h1{
   color:white;
   text-align: center;
+  
+  margin: 0px 0px;
+  font-family: 'Poppins',sans-serif;
+  text-shadow: 2px 2px 2px black;
+
 }
+
 .bg-image {
  
   background-size: cover;
--- vue/src/views/WorkoutHistory.vue
+++ vue/src/views/WorkoutHistory.vue
@@ -3,6 +3,8 @@
      style="background-image: url('https://images.rawpixel.com/image_1000/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8yMV9waG90b19vZl9maXRuZXNzX2d5bV9iYWNrZ3JvdW5kX2NvcHlfc3BhY2VfZThiOGMxMTAtYzRhOC00YjlmLTk2OGEtZWUzNDVlZGQwYzUwXzEuanBn.jpg');" >
   
   <div class="history">
+    <br>
+    <br>
       <h1>Workout History</h1>
   </div>
   </div>
@@ -15,6 +17,15 @@ export default {
 </script>
 
 <style scoped>
+h1{
+  color:white;
+  text-align: center;
+  
+  margin: 0px 0px;
+  font-family: 'Poppins',sans-serif;
+  text-shadow: 2px 2px 2px black;
+
+}
 .bg-image {
  
   background-size: cover;
