4ba7e1f91c735cf6d985ec8c5e69d2bd98ee5d75
Jose <rev.jose66@gmail.com>
2023-10-03 20:23:06 -0700

    avatar and username

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,8 +3,13 @@
      style="background-image: url('https://images.rawpixel.com/image_1000/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlN19waG90b2dyYXBoeV9vZl9tb2Rlcm5fZml0bmVzc19yb29tX21pbmltYWxfOGtfMjdjNmZkYjctNGQxMS00Yjc1LWJkODQtZThjZTBiMTY4NmY3XzEuanBn.jpg');" >
   <div class="home"  > 
     <h1>Welcome </h1>
+
+
+ <img src="avatar.png" alt="Avatar" class="Avatar">
+
+
     <user-data />
-       
+      
 
 
     
@@ -32,4 +37,14 @@ h1{
   color: white;
 }
 
+.Avatar {
+   display: flex;
+  
+  width: 110px;
+  height: 110px;
+margin-top: 3%;
+ margin-left: auto;
+ margin-right: auto;
+
+}
 </style>
